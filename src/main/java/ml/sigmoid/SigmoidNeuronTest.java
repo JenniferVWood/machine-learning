9c6a8d10a10fc589d050c12be26a2c70c59ec5b5
github@davewhoyt.com <4ec2c9272bdd3234f3bdcb53fbdf2fe1c09011db>
2017-08-29 10:07:30 -0500

    Added a little test that compares behavior of a sigmoid neuron with a perceptron.

---
+++ src/main/java/ml/sigmoid/SigmoidNeuronTest.java
@@ -0,0 +1,40 @@
+package ml.sigmoid;
+
+import ml.perceptron.Input;
+import ml.perceptron.Perceptron;
+
+public class SigmoidNeuronTest {
+    public static void main(String[] args) {
+//        testOneNeuron();
+        testOnePerceptron();
+    }
+
+    private static void testOneNeuron() {
+        SigmoidNeuron neuron = new SigmoidNeuron(1.0);
+
+
+        for (double value = -2.0; value < 2.0; value += 0.1) {
+            for (double weight = -2.0; weight < 2.0; weight += 0.1) {
+                Input input1 = new Input(value);
+//                Input input2 = new Input(value -0.1);
+
+                neuron.addInput(input1, weight);
+//                neuron.addInput(input2, weight);
+
+                System.out.println(/*"" + (value - 0.1) + " " +*/ value + " " + weight + ": " + neuron.value());
+            }
+        }
+    }
+
+    private static void testOnePerceptron() {
+        Perceptron neuron = new Perceptron(1.0);
+
+        for (double value = -2.1; value < 2.0; value += 0.1) {
+            for (double weight = -2.0; weight < 2.0; weight += 0.1) {
+                Input input1 = new Input(value);
+                neuron.addInput(input1, weight);
+                System.out.println(value + " " + weight + ": " + neuron.value());
+            }
+        }
+    }
+}
