e284197ca3969f5227e9466f2390c9d5beddcc18
david <david@davidwhoyt.com>
2018-01-29 17:52:59 -0600

    now gets weather concurrently.  Todo: better error handling.

--- data-collection-service/src/github.com/buskersguidetotheuniverse.org/main/run.go
+++ data-collection-service/src/github.com/buskersguidetotheuniverse.org/main/run.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/buskersguidetotheuniverse.org/noaa"
-	"github.com/buskersguidetotheuniverse.org/hbase"
 	"github.com/buskersguidetotheuniverse.org/types"
 	"flag"
 	"log"
 	"os"
-	"reflect"
+	"sync"
+	"github.com/buskersguidetotheuniverse.org/noaa"
+	"github.com/buskersguidetotheuniverse.org/hbase"
 )
+
+var wg sync.WaitGroup
 
 // Fetch the weather from a series of NOAA stations and save the results to a local hbase instance.
 func main() {
@@ -29,38 +31,23 @@ func main() {
 	log.Printf("-p: %v\n", *printWeather)
 	log.Printf("tail: %v\n", stations)
 
-
-	var chans [] chan int
-	for i := 0; i < numStations; i++ {
-		ch := make(chan int)
-		chans = append(chans, ch)
-		go processStation(stations[i], *printWeather, ch)
+	for _, station := range stations {
+		s := station
+		log.Printf("station: %v", s)
+		wg.Add(1)
+		go func(station string, pw bool) {
+			defer wg.Done()
+			processStation(station, pw)
+			log.Println("processing returned.")
+		}(s, *printWeather)
 	}
 
-
-	// I wish I really understood how this worked...
-	// taken from https://play.golang.org/p/8zwvSk4kjx
-	// and https://stackoverflow.com/questions/19992334/how-to-listen-to-n-channels-dynamic-select-statement
-	cases := make([]reflect.SelectCase, len(chans))
-	for i, ch := range chans {
-		cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
-	}
-
-	remaining := len(cases)
-	for remaining > 0 {
-		chosen, value, ok := reflect.Select(cases)
-		if !ok {
-			// The chosen channel has been closed, so zero out the channel to disable the case
-			cases[chosen].Chan = reflect.ValueOf(nil)
-			remaining -= 1
-			continue
-		}
-
-		log.Printf("Read from channel %#v and received %s\n", chans[chosen], value.String())
-	}
+	log.Println("waiting for all threads to return.")
+	wg.Wait()
 }
 
-func processStation(station string, printWeather bool, ch chan int) {
+func processStation(station string, printWeather bool) {
+	log.Println("processing..." + station)
 	currentConditions, err := noaa.CurrentConditions(station)
 	if err != nil {
 		log.Printf("WARN: couldn't look up weather for %v:  %v", station, err)
@@ -69,6 +56,7 @@ func processStation(station string, printWeather bool, ch chan int) {
 		return
 	}
 
+	//TODO: Use ErrorGroup or whatever instead.
 	err = hbase.SaveObservation(&currentConditions)
 	if err != nil {
 		log.Fatalf("Error saving weather to HBase: %v", err)
@@ -79,7 +67,8 @@ func processStation(station string, printWeather bool, ch chan int) {
 		printCurrentConditions(&currentConditions)
 	}
 
-	ch <- 1
+	log.Println("done processing")
+
 }
 
 func printCurrentConditions(currentConditions *types.CurrentConditionsResponse) {
