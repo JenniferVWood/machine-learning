d79d3af20a88e369466832f7cb580f5b5c20d2b6
david <david@davidwhoyt.com>
2018-02-11 10:12:08 -0600

    begin moving code to service package (or type?)

--- data-collection-service/src/github.com/buskersguidetotheuniverse.org/main/run.go
+++ data-collection-service/src/github.com/buskersguidetotheuniverse.org/main/run.go
@@ -7,6 +7,7 @@ import (
 	"github.com/buskersguidetotheuniverse.org/hbase"
 	"github.com/buskersguidetotheuniverse.org/noaa"
 	"github.com/buskersguidetotheuniverse.org/openei"
+	"github.com/buskersguidetotheuniverse.org/service"
 	"github.com/buskersguidetotheuniverse.org/types"
 	"log"
 	"os"
@@ -83,7 +84,7 @@ func handleStationLiterals(stations []string, printWeather *bool, wg *sync.WaitG
 			PrintWeather: *printWeather,
 			StationId:    s,
 		}
-		go ProcessStation(&request, wg)
+		go service.ProcessStation(&request, wg)
 	}
 }
 
@@ -103,51 +104,7 @@ func handleLatLong(queryLocation *types.Geometry, printWeather *bool, wg *sync.W
 			PrintWeather:      *printWeather,
 			QueryLocation:     *queryLocation,
 		}
-		go ProcessStation(&request, wg)
+		go service.ProcessStation(&request, wg)
 	}
-
-}
-
-// TODO: This is both NOAA and HBase code.  Maybe it belongs somewhere else?
-// Download current observations for a given station, and persist the results to our HBase instance.
-// For now, we discard information about the actual station.  Most of what we need is embedded in the conditions response.
-//
-func ProcessStation(request *types.ProcessStationRequest, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	currentConditions, err := noaa.CurrentConditions(request.StationProperties.StationIdentifier)
-	if err != nil {
-		log.Printf("WARN: couldn't look up weather for %v:  %v", request.StationProperties.StationIdentifier, err)
-
-		// not a fatal error, but obviously we don't want to try to persist the result
-		return
-	}
-
-	currentConditions.Props.QueryLocation = request.QueryLocation
-
-	//TODO: Use ErrorGroup or whatever instead.
-	err = hbase.SaveObservation(&currentConditions)
-	if err != nil {
-		log.Fatalf("Error saving weather to HBase: %v", err)
-		os.Exit(1)
-	}
-
-	if request.PrintWeather {
-		printCurrentConditions(&currentConditions)
-	}
-
-	//log.Println("done processing")
-
-}
-
-func printCurrentConditions(currentConditions *types.CurrentConditionsResponse) {
-	fmt.Printf("Current Conditions:\n")
-	fmt.Println(currentConditions.Props.Station)
-	fmt.Println(currentConditions.Props.Timestamp)
-	//fmt.Println(currentConditions.Props.BarometricPressure)
-	fmt.Println(currentConditions.Props.Temperature)
-	fmt.Println(currentConditions.Props.WindSpeed)
-	fmt.Println(currentConditions.Props.WindDirection)
-	fmt.Println(currentConditions.Props.TextDescription)
 
 }
---
+++ data-collection-service/src/github.com/buskersguidetotheuniverse.org/service/service.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"fmt"
+	"github.com/buskersguidetotheuniverse.org/hbase"
+	"github.com/buskersguidetotheuniverse.org/noaa"
+	"github.com/buskersguidetotheuniverse.org/types"
+	"log"
+	"os"
+	"sync"
+)
+
+type Service struct {
+}
+
+// TODO: This is both NOAA and HBase code.  Maybe it belongs somewhere else?
+// Download current observations for a given station, and persist the results to our HBase instance.
+// For now, we discard information about the actual station.  Most of what we need is embedded in the conditions response.
+//
+func ProcessStation(request *types.ProcessStationRequest, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	currentConditions, err := noaa.CurrentConditions(request.StationProperties.StationIdentifier)
+	if err != nil {
+		log.Printf("WARN: couldn't look up weather for %v:  %v", request.StationProperties.StationIdentifier, err)
+
+		// not a fatal error, but obviously we don't want to try to persist the result
+		return
+	}
+
+	currentConditions.Props.QueryLocation = request.QueryLocation
+
+	//TODO: Use ErrorGroup or whatever instead.
+	err = hbase.SaveObservation(&currentConditions)
+	if err != nil {
+		log.Fatalf("Error saving weather to HBase: %v", err)
+		os.Exit(1)
+	}
+
+	if request.PrintWeather {
+		printCurrentConditions(&currentConditions)
+	}
+
+	//log.Println("done processing")
+
+}
+
+func printCurrentConditions(currentConditions *types.CurrentConditionsResponse) {
+	fmt.Printf("Current Conditions:\n")
+	fmt.Println(currentConditions.Props.Station)
+	fmt.Println(currentConditions.Props.Timestamp)
+	//fmt.Println(currentConditions.Props.BarometricPressure)
+	fmt.Println(currentConditions.Props.Temperature)
+	fmt.Println(currentConditions.Props.WindSpeed)
+	fmt.Println(currentConditions.Props.WindDirection)
+	fmt.Println(currentConditions.Props.TextDescription)
+
+}
